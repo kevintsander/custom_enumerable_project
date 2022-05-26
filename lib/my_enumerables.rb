8f7a6d4faeea0bb9754c9b049f3f24120876599f
Kevin Sander <kevintsander@gmail.com>
2022-05-25 22:53:24 -0400

    implemented my_ each, select, all?, any?, none?

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,41 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    index = 0
+    my_each do |item|
+      yield(item, index)
+      index += 1
+    end
+  end
+
+  def my_select
+    arr = []
+    my_each do |item|
+      arr.push(item) if yield(item) == true
+    end
+    arr
+  end
+
+  def my_all?
+    my_each do |item|
+      return false if yield(item) == false
+    end
+    true
+  end
+
+  def my_any?
+    my_each do |item|
+      return true if yield(item) == true
+    end
+    false
+  end
+
+  def my_none?
+    my_each do |item|
+      return false if yield(item) == true
+    end
+    true
+  end
 end
 
 # You will first have to define my_each
@@ -8,4 +44,11 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    while (index ||= 0) < length
+      yield(self[index])
+      index += 1
+    end
+    self
+  end
 end
